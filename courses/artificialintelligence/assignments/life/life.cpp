ec27036bb9621161ade831762de872e5adf99b9e
Wang Grundy <2101005@uad.ac.uk>
2023-09-21 01:29:42 -0400

    FEATURE: conway rules

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -4,20 +4,25 @@
 
 //structs
 struct Vector2f{
-  Vector2f(int x, int y, bool alive) : x(x), y(y), alive(alive){};
+  Vector2f(int x, int y) : x(x), y(y){};
   float x = 0;
   float y = 0;
-  bool alive = false;
+  Vector2f Up(){ return Vector2f(x,y -1);};
+  Vector2f Down(){ return Vector2f(x,y+1);};
+  Vector2f Left(){ return Vector2f(x,y-1);};
+  Vector2f Right(){ return Vector2f(x+1,y);};
 };
 
 //prototypes
 std::vector<std::vector<int>> CreateMap(const int& mapSize, const std::vector<Vector2f>& addTiles);
 void AddTiles(std::vector<std::vector<int>>&, const std::vector<Vector2f>&);
 void PrintMap(const std::vector<std::vector<int>>& map, const int& mapSize);
+int CountNeighbors(std::vector<std::vector<int>>& map, Vector2f point);
+bool Get(std::vector<std::vector<int>>& map, Vector2f point);
 
 int main(){
 
-    Vector2f tile1(3,3, true);
+    Vector2f tile1(3,3);
     std::vector<Vector2f> addTiles;
     addTiles.push_back(tile1);
 
@@ -74,3 +79,89 @@ void PrintMap(const std::vector<std::vector<int>>& map, const int& mapSize){
   }
 }
 
+int CountNeighbors(std::vector<std::vector<int>>& map, Vector2f point) {
+  //TOP LEFT IS 0,0
+  int neighbourCount = 0;
+  Vector2f top = point.Up().Left();
+  Vector2f middle = point.Left();
+  Vector2f bottom = point.Down().Left();
+
+  if(Get(map,point.Up())){
+    neighbourCount++;
+  }
+  if(Get(map,point.Down())){
+    neighbourCount++;
+  }
+
+  if(Get(map,point.Up().Left())){
+    neighbourCount++;
+  }
+  if(Get(map,point.Left())){
+    neighbourCount++;
+  }
+  if(Get(map,point.Down().Left())){
+    neighbourCount++;
+  }
+
+  if(Get(map,point.Up().Right())){
+    neighbourCount++;
+  }
+  if(Get(map,point.Right())){
+    neighbourCount++;
+  }
+  if(Get(map,point.Down().Right())){
+    neighbourCount++;
+  }
+
+  return neighbourCount;
+}
+
+bool Get(std::vector<std::vector<int>>& map, Vector2f point){
+  if(map[point.x][point.y]){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+void Step(std::vector<std::vector<int>>& map, const int& mapSize) {
+  Vector2f currentLocation = {0, 0};
+  int numberOfNeighbours = 0;
+  bool isAlive = false;
+
+  for (int i = 0; i < mapSize; i++) {
+    for (int j = 0; j < mapSize; j++) {
+      currentLocation = Vector2f(j, i);
+      numberOfNeighbours = CountNeighbors(map, currentLocation);
+      isAlive = Get(map, currentLocation);
+
+      //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+      //Any live cell with two or three live neighbours lives on to the next generation.
+      //Any live cell with more than three live neighbours dies, as if by overpopulation.
+      //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+//
+//      if (isAlive) {  // if alive
+//        //Each cell with one or no neighbors dies, as if by solitude.
+//        if (numberOfNeighbours <= 1) {
+//          world.SetNext(currentLocation, false);
+//        }
+//        // Each cell with two or three neighbors survives.
+//        else if (numberOfNeighbours == 2 || numberOfNeighbours == 3) {
+//          world.SetNext(currentLocation, true);
+//        }
+//        // Each cell with four or more neighbors dies, as if by overpopulation.
+//        else if (numberOfNeighbours >= 4) {
+//          world.SetNext(currentLocation, false);
+//        }
+//
+//      }
+//      else if (!isAlive) {  // if dead
+//        // Each cell with three neighbors becomes populated.
+//        if (numberOfNeighbours == 3) {
+//          world.SetNext(currentLocation, true);
+//        }
+//      }
+    }
+  }
+}
