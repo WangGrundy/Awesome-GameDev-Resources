5f5a41f47032d13aef7b23bf493ac21034a8ca99
Wang Grundy <2101005@uad.ac.uk>
2023-09-22 13:33:51 -0400

    working on this

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -13,36 +13,43 @@ struct Vector2f{
   Vector2f Right(){ return Vector2f(x+1,y);};
 };
 
+using std::vector;
+using std::cout;
+
 //prototypes
 std::vector<std::vector<int>> CreateMap(const int& mapSize, const std::vector<Vector2f>& addTiles);
 void AddTiles(std::vector<std::vector<int>>&, const std::vector<Vector2f>&);
 void PrintMap(const std::vector<std::vector<int>>& map, const int& mapSize);
 int CountNeighbors(std::vector<std::vector<int>>& map, Vector2f point);
 bool Get(std::vector<std::vector<int>>& map, Vector2f point);
+void Step(vector<vector<int>>& currentMap, vector<Vector2f>& addTiles, const int& mapSize);
 
 int main(){
 
     Vector2f tile1(3,3);
-    std::vector<Vector2f> addTiles;
+    vector<Vector2f> addTiles;
+    vector<Vector2f> addTiles2;
     addTiles.push_back(tile1);
 
     int mapSize = 5;
-    std::vector<std::vector<int>> map;
-    map = CreateMap(mapSize, addTiles);
+    vector<vector<int>> currentMap, nextMap;
+    currentMap = CreateMap(mapSize, addTiles);
+
+    nextMap = CreateMap(mapSize, addTiles2);
     //AddTiles(tilesAdded);
 
     //std::cout << map[0][0] << map[1][0];
 
-    PrintMap(map, mapSize);
+    PrintMap(currentMap, mapSize);
 
   return 0;
 };
 
 //prototypes
-std::vector<std::vector<int>> CreateMap(const int& mapSize, const std::vector<Vector2f>& addTiles){
+vector<vector<int>> CreateMap(const int& mapSize, const vector<Vector2f>& addTiles){
 
   //create 2D vector to store map results and initiate size of 2D vector
-  std::vector<std::vector<int>> map(mapSize, std::vector<int>(mapSize));
+  vector<vector<int>> map(mapSize, vector<int>(mapSize));
 
   int counter = 0;
 
@@ -58,14 +65,14 @@ std::vector<std::vector<int>> CreateMap(const int& mapSize, const std::vector<Ve
   return map;
 }
 
-void AddTiles(std::vector<std::vector<int>>& map, const std::vector<Vector2f>& tiles){
+void AddTiles(vector<vector<int>>& map, const vector<Vector2f>& tiles){
   for(int i = 0; i < tiles.size(); i++){
     map[tiles[i].x][tiles[i].y] = 1;
   }
 }
 
 //print map probably
-void PrintMap(const std::vector<std::vector<int>>& map, const int& mapSize){
+void PrintMap(const vector<vector<int>>& map, const int& mapSize){
   for (int y = 0; y < mapSize; y++){
 
     for (int x = 0; x < mapSize; x++){
@@ -79,7 +86,7 @@ void PrintMap(const std::vector<std::vector<int>>& map, const int& mapSize){
   }
 }
 
-int CountNeighbors(std::vector<std::vector<int>>& map, Vector2f point) {
+int CountNeighbors(vector<vector<int>>& map, Vector2f point) {
   //TOP LEFT IS 0,0
   int neighbourCount = 0;
   Vector2f top = point.Up().Left();
@@ -116,7 +123,7 @@ int CountNeighbors(std::vector<std::vector<int>>& map, Vector2f point) {
   return neighbourCount;
 }
 
-bool Get(std::vector<std::vector<int>>& map, Vector2f point){
+bool Get(vector<vector<int>>& map, Vector2f point){
   if(map[point.x][point.y]){
     return true;
   }
@@ -125,43 +132,36 @@ bool Get(std::vector<std::vector<int>>& map, Vector2f point){
   }
 }
 
-void Step(std::vector<std::vector<int>>& map, const int& mapSize) {
+void Step(vector<vector<int>>& currentMap, vector<Vector2f>& addTiles, const int& mapSize) {
   Vector2f currentLocation = {0, 0};
   int numberOfNeighbours = 0;
   bool isAlive = false;
 
-  for (int i = 0; i < mapSize; i++) {
-    for (int j = 0; j < mapSize; j++) {
-      currentLocation = Vector2f(j, i);
-      numberOfNeighbours = CountNeighbors(map, currentLocation);
-      isAlive = Get(map, currentLocation);
+  addTiles.clear(); // clear the array of added tiles for this time
+
+  for (int y = 0; y < mapSize; y++) {
+    for (int x = 0; x < mapSize; x++) {
+      currentLocation = Vector2f(x, y);
+      numberOfNeighbours = CountNeighbors(currentMap, currentLocation);
+      isAlive = Get(currentMap, currentLocation);
 
       //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
       //Any live cell with two or three live neighbours lives on to the next generation.
       //Any live cell with more than three live neighbours dies, as if by overpopulation.
       //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-//
-//      if (isAlive) {  // if alive
-//        //Each cell with one or no neighbors dies, as if by solitude.
-//        if (numberOfNeighbours <= 1) {
-//          world.SetNext(currentLocation, false);
-//        }
-//        // Each cell with two or three neighbors survives.
-//        else if (numberOfNeighbours == 2 || numberOfNeighbours == 3) {
-//          world.SetNext(currentLocation, true);
-//        }
-//        // Each cell with four or more neighbors dies, as if by overpopulation.
-//        else if (numberOfNeighbours >= 4) {
-//          world.SetNext(currentLocation, false);
-//        }
-//
-//      }
-//      else if (!isAlive) {  // if dead
-//        // Each cell with three neighbors becomes populated.
-//        if (numberOfNeighbours == 3) {
-//          world.SetNext(currentLocation, true);
-//        }
-//      }
+
+      if (isAlive) {  // if alive
+        // Each cell with two or three neighbors survives.
+        if (numberOfNeighbours == 2 || numberOfNeighbours == 3) {
+          addTiles.push_back(Vector2f(x,y));
+        }
+      }
+      else if (!isAlive) {  // if dead
+        // Each cell with three neighbors becomes populated.
+        if (numberOfNeighbours == 3) {
+          addTiles.push_back(Vector2f(x,y));
+        }
+      }
     }
   }
 }
--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,46 @@
 // add your imports here
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
 int main(){
-  // code here
+
+  //unsigned ints are 32 bits
+  unsigned int currentNumber = 1;
+  unsigned int seed = 1;
+  unsigned int tempNumber = 0;
+  int numbersGenerated = 5;
+  unsigned upperRange = 0, lowerRange = 0;
+
+  cout << "Please enter a seed: ";
+  cin >> seed;
+  cout << endl;
+  seed = currentNumber;
+
+  for(int i = 0; i < numbersGenerated; i++){
+    cout << "---------------------------------------------------------------\n";
+    cout << "seed is: " << seed << endl;
+
+    //  xorshift the value by 13 bits to the left;
+    tempNumber = seed << 13;
+    currentNumber ^= tempNumber;
+
+    //  xorshift the value by 17 bits to the right;
+    tempNumber = seed >> 17;
+    currentNumber ^= tempNumber;
+
+    //  xorshift the value by 5 bits to the left;
+    tempNumber = seed >> 5;
+    currentNumber ^= tempNumber;
+
+    //clamp final value
+    currentNumber = lowerRange + (currentNumber % (upperRange - lowerRange + 1));
+
+    cout << endl << "Random number generated: " << currentNumber << endl;
+
+    //set seed for next round
+    seed = currentNumber;
+  }
 }
