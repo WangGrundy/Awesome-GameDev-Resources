f3d18fe8f386f093b43b21ef9523b3a30a242aee
Wang Grundy <2101005@uad.ac.uk>
2023-09-23 19:41:13 -0400

    finished rng

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,11 +1,11 @@
 // add your imports here
 #include <iostream>
 #include <bitset>
+#include <sstream>
+#include <vector>
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::bitset;
+using std::bitset, std::string, std::cin, std::endl, std::cout, std::getline, std::istringstream;
+using std::vector;
 
 int main(){
 
@@ -16,19 +16,56 @@ int main(){
   int numbersGenerated = 5;
   unsigned upperRange = 0, lowerRange = 0;
 
-  cout << "Please enter a seed: ";
-  cin >> seed;
-  cout << endl;
-  currentNumber = seed; //initially
+//  cout << "Please enter a seed: ";
+//  cin >> seed;
+//  cout << endl;
+//  currentNumber = 0; //initially
+//
+//  cout << "Please enter a upperRange:";
+//  cin >> upperRange;
+//
+//  cout << "Please enter a lowerRange:";
+//  cin >> lowerRange;
+//
+//  cout << "Please enter the numbers you want to generate:";
+//  cin >> numbersGenerated;
 
-  cout << "Please enter a upperRange:";
-  cin >> upperRange;
+  cout << "INPUT: Seed, Numbers generated, lower range, upper range:" << endl;
 
-  cout << "Please enter a lowerRange:";
-  cin >> lowerRange;
+  vector<int> inputVec;
+  string input;
+  unsigned int tempInt;
+  getline(std::cin, input); // Read a line of input
 
-  cout << "Please enter the numbers you want to generate:";
-  cin >> numbersGenerated;
+  string app;
+  int counter = 0;
+
+  for(int i = 0; i < input.size(); i++){ //loop through string
+    if(input[i] == ' '){ //if we have a space, start adding numbers
+      tempInt = stoi(app, 0, 10); //string to int (string, start, base)
+      inputVec.push_back(tempInt); //add to vector of unsigned ints
+      app.clear(); //clear string for later use
+      continue;
+    }
+    app += input[i];
+  }
+
+  //we miss last number in string, so if anything is left inside app, then we add it to our vec
+  if (!app.empty()) {
+    tempInt = stoi(app, 0, 10);
+    inputVec.push_back(tempInt);
+  }
+
+  //set prompts
+  seed = inputVec[0];
+  numbersGenerated = inputVec[1];
+  lowerRange = inputVec[2];
+  upperRange = inputVec[3];
+
+  cout << "seed: " << inputVec[0] << endl;
+  cout << "numbers generated: " << inputVec[1] << endl;
+  cout << "lower range: " << inputVec[2] << endl;
+  cout << "upper range: " << inputVec[3] << endl;
 
   cout << endl;
 
@@ -53,6 +90,7 @@ int main(){
     seed = currentNumber;
 
     //clamp final value
+    cout << "clamped value between: " << inputVec[2] << " and " << inputVec[3] << endl;
     cout << "Value before clamping: " << currentNumber << endl;
     currentNumber = lowerRange + (currentNumber % (upperRange - lowerRange + 1));
 
